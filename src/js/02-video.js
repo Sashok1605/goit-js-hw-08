eec452921de9e3d29c193a30b570a282944544b0
Aleksandr Kulyk <ale.kulyk@gmail.com>
2021-10-04 20:28:38 +0300

    исправил 2 и 3 задание

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,10 +6,12 @@ const iframe = document.querySelector('iframe');
     const player = new Vimeo(iframe);
 
     let time = localStorage.getItem("videoplayer-current-time") || 0;
+    
+    player.on('timeupdate', throttle(data => {
+        localStorage.setItem("videoplayer-current-time", data.seconds)}
+    , 1000));
+
     player.setCurrentTime(time);
-    player.on('timeupdate', throttle((data) => {
-        localStorage.setItem("videoplayer-current-time", data.seconds)})
-    , 1000);
     
 
     
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,30 +1,42 @@
 import throttle from 'lodash.throttle';
 
 const inputForm = document.querySelector('.feedback-form');
-const inputPassword = inputForm.querySelector("textarea");
 const inputEmail = inputForm.querySelector("input");
+const inputMassage = inputForm.querySelector("textarea");
+
+
+let valueInputs = {
+    email: '',
+    message: ''
+};
 
 let getInputs = localStorage.getItem("feedback-form-state");
-let localInputs = JSON.parse(getInputs);
 
-
-if (localInputs) {
-    inputEmail.value = parsedInputs.email;
-    inputPassword.value = parsedInputs.password;
+if (getInputs) {
+    valueInputs = JSON.parse(getInputs);
+    inputEmail.value = valueInputs.email;
+    inputMassage.value = valueInputs.massage;
 }
+
 inputForm.addEventListener("input", throttle(enterInput, 500));
 inputForm.addEventListener("submit", enterSubmit);
 function enterInput() {
     const obj = {
         email: inputForm.elements.email.value,
-        password: inputForm.elements.password.value
+        message: inputForm.elements.message.value
     }
     localStorage.setItem("feedback-form-state", JSON.stringify(obj));
 }
 function enterSubmit(e) {
     e.preventDefault();
     getInputs = localStorage.getItem("feedback-form-state");
-    localInputs = JSON.parse(getInputs);
+    if(getInputs) {
+    valueInputs = JSON.parse(getInputs);
+    inputEmail.value = valueInputs.email;
+    inputMassage.value = valueInputs.massage;
+    }
+    console.log(valueInputs);
+    
     e.currentTarget.reset();
     localStorage.removeItem("feedback-form-state");
-    }
+}
